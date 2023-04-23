170469fe8417d5da4d8960a56b6fc695575482f1
Hamidjon03 <forevalm@gmail.com>
2023-04-23 11:35:15 +0500

    stack

---
+++ Algoritm/9.Stack.js
@@ -0,0 +1,40 @@
+// Stack va Rekursiya
+
+class Stack {
+  stack = [];
+
+  // bo'sh ekanligini tekshirish
+  isEmpty(){
+    console.log(this.stack.length === 0)
+  }
+
+  // element qo'shish
+  push(data){
+    this.stack.unshift(data)
+    console.log("Item added")
+  }
+
+  // element sug'urib olish
+  pop() {
+    if(this.stack.length === 0){
+      console.log('This stack is Empty')
+    }
+    this.stack.shift()
+    console.log("Item deleted")
+  }
+
+  // Eng ustki elementni ko'rish
+  peek(){
+    if (this.stack.length === 0) {
+      console.log('This stack is Empty')
+    }
+    console.log(this.stack[0])
+  }
+
+}
+
+
+let stack = new Stack()
+// stack.isEmpty()
+stack.push(7)
+stack.peek()

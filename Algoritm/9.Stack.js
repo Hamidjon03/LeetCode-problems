80a0a47c063b95ef2a253f2c562e6f2d530c9a88
Hamidjon03 <forevalm@gmail.com>
2023-05-02 23:12:21 +0500

    added something

--- Algoritm/9.Stack.js
+++ Algoritm/9.Stack.js
@@ -38,3 +38,16 @@ let stack = new Stack()
 // stack.isEmpty()
 stack.push(7)
 stack.peek()
+
+
+
+
+function fact(x){
+  if(x == 1){
+    return '1'
+  } else {
+    return x * fact(x-1)
+  }
+}
+
+console.log(fact(5))
